5e1a62a735be73aa997ef125d131b59ca1cd7f49
lianilychee <liani.lye@students.olin.edu>
2014-09-14 19:15:46 -0400

    Hw: compare user input values

---
+++ hw2/compare.py
@@ -0,0 +1,23 @@
+'''
+Liani Lye, HW 02
+SoftDes Fall '14
+Due 09.16.14
+
+Write a compare function that returns 1 if x > y, 0 if x == y, and -1 if x < y
+'''
+
+x = int(raw_input("enter value for x: "));
+y = int(raw_input("enter value for y: "));
+
+def compare():
+	if x > y:
+		print 1;
+		return 1;
+	if x == y:
+		print 0;
+		return 0;
+	if x < y:
+		print -1;
+		return -1;
+
+compare();
