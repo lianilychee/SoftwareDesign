82adc59a22d5efad4b100aeeb415c9d5027b26f9
lianilychee <liani.lye@students.olin.edu>
2014-09-22 20:26:32 -0400

    acquired api key

--- hw3/pobama.py
+++ hw3/pobama.py
@@ -1,11 +1,18 @@
 # API call business
+# API key bSJDOQnZGZxXHSpHjCTnh1Oxf
 
-print "hello world"
+import time
 
-from pattern.web import Twitter
+def stream():
 
-s = Twitter().stream('#fail')
-for i in range(10):
-	time.sleep(1) 
-	s.update(bytes=1024)
-	print s[-1].text if s else ''
+	print "hello world"
+
+	from pattern.web import Twitter
+
+	s = Twitter().stream('#fail')
+	for i in range(10):
+		time.sleep(1) 
+		s.update(bytes=1024)
+		print s[-1].text if s else ''
+
+stream()
