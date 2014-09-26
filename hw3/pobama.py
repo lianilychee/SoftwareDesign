d862d01f13404ad692c093e6512d0e1da6be9d49
lianilychee <liani.lye@students.olin.edu>
2014-09-26 13:58:50 -0400

    logging correctly

--- hw3/pobama.py
+++ hw3/pobama.py
@@ -10,15 +10,9 @@ import codecs
 from pattern.web import Twitter
 from indicoio import sentiment as sent
 
+time = datetime.datetime.now()
+fileSave = codecs.open( str(time) +'.txt','w','utf-8' )
 
-def filename():
-	time = datetime.datetime.now()
-	fileSave = codecs.open( str(time) +'.txt','w','utf-8' )
-
-filename()
-
-
-fileSave = codecs.open('tweets.txt','w','utf-8')
 
 def search():
 
@@ -28,25 +22,12 @@ def search():
 	t = Twitter()
 	i = None
 	for j in range(1):
-		for tweet in t.search(query, start = i, count = 1):
+		for tweet in t.search(query, start = i, count = 10):
 			# print "text: " + tweet.text
 
-			test = sent(tweet.text)
+			test = str(sent(tweet.text)) + "\n"
 			print test
-			print
+
+			fileSave.write(test)
 
 search()
-
-
-# def stream():
-# 	print "hello world"
-
-# 	from pattern.web import Twitter
-
-# 	s = Twitter().stream('#fail')
-# 	for i in range(10):
-# 		time.sleep(1) 
-# 		s.update(bytes=1024)
-# 		print s[-1].text if s else ''
-
-# stream()
