2a038a5838c85e23184b46c4c80480b5ebbe28ef
lianilychee <liani.lye@students.olin.edu>
2014-09-25 20:52:02 -0400

    changed sentiment package from pattern to indico

--- hw3/pobama.py
+++ hw3/pobama.py
@@ -1,9 +1,16 @@
+#encoding: utf-8
+
 # API call business
 # API key bSJDOQnZGZxXHSpHjCTnh1Oxf
 
+
 import time
+import pickle
+# import codecs
 from pattern.web import Twitter
-from pattern.en import sentiment
+from indicoio import sentiment as sent
+
+# thing = codecs.open('tweets.txt','w','utf-8')
 
 query = str(raw_input("enter search query: "))
 # username = str(raw_input("enter username: "))
@@ -14,15 +21,13 @@ def search(query):
 	i = None
 	for j in range(1):
 		for tweet in t.search(query, start = i, count = 1):
-			# print tweet
-			# print "author: " + tweet.author
 			print "text: " + tweet.text
-			# print "date: " + tweet.date
-			# i = tweet.id
 
-	test = sentiment (tweet.text)
-	print test
-	print test[0]
+			test = sent(tweet.text)
+			print test
+			print
+
+			# thing.write(tweet.text  + "\n \n");
 
 
 search(query);
