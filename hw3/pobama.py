587b0528ff54fce2786350e71c3b5b4bd8321ae3
lianilychee <liani.lye@students.olin.edu>
2014-09-26 13:52:59 -0400

    filename generating

--- hw3/pobama.py
+++ hw3/pobama.py
@@ -5,32 +5,37 @@
 
 
 import time
-import pickle
-# import codecs
+import datetime
+import codecs
 from pattern.web import Twitter
 from indicoio import sentiment as sent
 
-# thing = codecs.open('tweets.txt','w','utf-8')
 
-query = str(raw_input("enter search query: "))
-# username = str(raw_input("enter username: "))
+def filename():
+	time = datetime.datetime.now()
+	fileSave = codecs.open( str(time) +'.txt','w','utf-8' )
 
-def search(query):
+filename()
+
+
+fileSave = codecs.open('tweets.txt','w','utf-8')
+
+def search():
+
+	query = str(raw_input("enter search query: "))
+	# username = str(raw_input("enter username: "))
 
 	t = Twitter()
 	i = None
 	for j in range(1):
 		for tweet in t.search(query, start = i, count = 1):
-			print "text: " + tweet.text
+			# print "text: " + tweet.text
 
 			test = sent(tweet.text)
 			print test
 			print
 
-			# thing.write(tweet.text  + "\n \n");
-
-
-search(query);
+search()
 
 
 # def stream():
