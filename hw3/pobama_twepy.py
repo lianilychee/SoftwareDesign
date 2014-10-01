34705ea0b387756018a484ece6a376197e9a61e2
lianilychee <liani.lye@students.olin.edu>
2014-10-01 10:09:55 -0400

    got common importing

--- hw3/pobama_twepy.py
+++ hw3/pobama_twepy.py
@@ -1,4 +1,5 @@
 import tweetpony
+from common import get_api
 
 consumer_key = 'zOWZuCJyqFw26egZTrY0ZL32G'
 consumer_secret = '7pPxVemHiEzarzPqwUEZrnNpUPk9mWRvmIiBGXtyQtcdI6i0UG'
@@ -7,10 +8,3 @@ access_token_secret = 'Ahx7aUjNcPaT6zBE8FfbVMAiilSh1fDM9g0Zhv9bPnHSE'
 
 api = tweetpony.API(consumer_key, consumer_secret, access_token, access_token_secret)
 
-user = api.user
-
-def testPrint():
-	# print "Hello, %s!" % user.screen_name
-	print user.followers_count
-
-testPrint()
