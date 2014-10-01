0fa23c91ad64b06e24be76bd594eba8c4d75e926
lianilychee <liani.lye@students.olin.edu>
2014-10-01 13:58:03 -0400

    final print line = result

--- hw3/pobama.py
+++ hw3/pobama.py
@@ -1,29 +1,26 @@
 #encoding: utf-8
 
-# API call business
-# API key bSJDOQnZGZxXHSpHjCTnh1Oxf
-
-
 import time
 import datetime
-# import codecs
+import codecs
 from pattern.web import Twitter
 import tweetpony
 from common import get_api
 from indicoio import sentiment as sent
 
-# Twitter API keys; user definition
+# Twitter API keys
 consumer_key = 'zOWZuCJyqFw26egZTrY0ZL32G'
 consumer_secret = '7pPxVemHiEzarzPqwUEZrnNpUPk9mWRvmIiBGXtyQtcdI6i0UG'
 access_token = '2508360470-zhIFODzznC0PdcAEa0oS6sY6p3Y9Wc8K1e6ZalO'
 access_token_secret = 'Ahx7aUjNcPaT6zBE8FfbVMAiilSh1fDM9g0Zhv9bPnHSE'
 api = tweetpony.API(consumer_key, consumer_secret, access_token, access_token_secret)
+
 user = api.user
 
 # Log file definition
-# time = str(datetime.datetime.now())
-# timeClipped = time[0:19]
-# fileSave = codecs.open( str(timeClipped) +'.txt','w','utf-8' )
+time = str(datetime.datetime.now())
+timeClipped = time[0:19]
+fileSave = codecs.open( str(timeClipped) +'.txt','w','utf-8' )
 
 # Pull down author follower count.
 def findFollows(author):
@@ -41,25 +38,38 @@ def findFollows(author):
 		follows = user.followers_count
 		return follows
 
+
 #  Pull down tweets and generate sentiment.
 def search():
 
 	query = str(raw_input("enter search query: "))
-	# username = str(raw_input("enter username: "))
-
 	t = Twitter()
 	i = None
-	# fileSave.write(query + "\n")
+	chances = 0
+	fileSave.write(query + "\n")
+
 	for j in range(1):
-		for tweet in t.search(query, start = i, count = 1):
-			# print "text: " + tweet.text
-			saveSent = str(sent(tweet.text)) + "\n"
-			# print saveSent
+		for tweet in t.search(query, start = i, count = 3):
+			
+			print tweet.text
 
-			# fileSave.write(saveSent)
+			# Calc tweet sentiment
+			sent_int = sent(tweet.text)
+			sent_str = str(sent_int)
+			print sent_str
 
-			author = tweet.author
-			print author
-			print findFollows(author)
+			# Calc author's follower count
+			follows_int = findFollows(tweet.author)
+			follows_str = str(sent_int)
+			print follows_str
+
+			# Calc chances; make cumulative
+			chances = chances + follows_int * sent_int
+
+			# File save
+			save = str(sent(tweet.text)) + "\n" + str(findFollows(author)) + "\n"
+			fileSave.write(save)
+
+			return chances
 
 search()
