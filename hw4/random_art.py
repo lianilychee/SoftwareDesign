afcba3bd33aa12d26a702763e10d35ea16fb48c0
lianilychee <liani.lye@students.olin.edu>
2014-10-03 15:07:42 -0400

    dictionary of math functions added

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -6,34 +6,38 @@ Random_art.py
 
 # you do not have to use these particular modules, but they may help
 from random import randint
+import math
 import Image
 
 min_depth = int(raw_input("min_depth: "))   # min amt of nesting
 max_depth = int(raw_input("max_depth: "))   # max amt of nesting
 a = int(raw_input("a: "))   # 1st input for build_random_function()
-b = int(raw_input("b: "))   # 2nd input for build_random_function() 
+b = int(raw_input("b: "))   # 2nd input for build_random_function()
+
+
 
 def build_random_function(min_depth, max_depth):
-    """ your doc string goes here
-    """
+	"""
+	Takes inputs recursion parameters min_depth, max_depth to generate random function.
+	"""
 
-    # your code goes here
+	rand_func = [['prod'],['sin_pi'],['cos_pi'],['a'],['b']]
 
 def evaluate_random_function(f, x, y):
-    """ your doc string goes here
-    """
+	""" your doc string goes here
+	"""
 
-    # your code goes here
+	# your code goes here
 
 def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
-    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
-        to the output interval [output_interval_start, output_interval_end].  The mapping
-        is an affine one (i.e. output = input*c + b).
-    
-        TODO: please fill out the rest of this docstring
-    """
-    # your code goes here
-    
+	""" Maps the input value that is in the interval [input_interval_start, input_interval_end]
+		to the output interval [output_interval_start, output_interval_end].  The mapping
+		is an affine one (i.e. output = input*c + b).
+	
+		TODO: please fill out the rest of this docstring
+	"""
+	# your code goes here
+	
 
 
 #your additional code and functions go here
