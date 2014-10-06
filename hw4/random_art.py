e437eaebc49019edb8e27ef2e1f4546d4d03529b
lianilychee <liani.lye@students.olin.edu>
2014-10-06 18:30:06 -0400

    build_random_function()

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Random_art.py
-@author: amonmillner, adapted from pruvolo work
+@author: lianilye, adapted from amonmillner's work (originally pruvolo)
 """
 
 # you do not have to use these particular modules, but they may help
@@ -9,11 +9,10 @@ from random import randint
 import math
 import Image
 
-min_depth = int(raw_input("min_depth: "))   # min amt of nesting
-max_depth = int(raw_input("max_depth: "))   # max amt of nesting
-a = int(raw_input("a: "))   # 1st input for build_random_function()
-b = int(raw_input("b: "))   # 2nd input for build_random_function()
-
+# min_depth = int(raw_input("min_depth: "))   # min amt of nesting
+# max_depth = int(raw_input("max_depth: "))   # max amt of nesting
+# a = int(raw_input("a: "))   # 1st input for build_random_function()
+# b = int(raw_input("b: "))   # 2nd input for build_random_function()
 
 
 def build_random_function(min_depth, max_depth):
@@ -21,7 +20,43 @@ def build_random_function(min_depth, max_depth):
 	Takes inputs recursion parameters min_depth, max_depth to generate random function.
 	"""
 
-	rand_func = [['prod'],['sin_pi'],['cos_pi'],['a'],['b']]
+	options = [['prod'],['sin_pi'],['cos_pi'],['a'],['b']]
+
+	# base case
+	if max_depth == 1:
+		return ['a']
+
+	# if min_depth reached, also choose base case
+	elif min_depth <= 1:
+
+		func_choice = options[randint(0,4)] # choose random func
+
+		# allows products to take two imputs
+		if function_choice == ['prod']:
+			return func_choice + [build_rand_func(min_depth-1, max_depth-1), build_rand_func(min_depth, max_depth)]
+
+		# if base case reached, end recursion
+		elif func_choice == ['a'] or func_choice == ['b']:
+			return func_choice
+
+		# otherwise, recurse as usual (single input)
+		else:
+			return func_choice + [build_rand_func(min_depth-1, max_depth-1)]
+
+	# if min_depth or max_depth not reached, loop again with min/max counters decremented
+	else:
+		func_choice = options[randint(0, 2)]
+
+		# allows products to take two inputs
+		if func_choice != ['prod']:
+			return func_choice + [(build_rand_func(min_depth-1, max_depth-1))]
+
+		# otherwise, recurse as usual
+		else:
+			return func_choice + [build_rand_func(min_depth-1, max_depth-1), build_rand_func(min_depth-1, max_depth-1)]
+
+	# print rand_func
+	# return rand_func
 
 def evaluate_random_function(f, x, y):
 	""" your doc string goes here
